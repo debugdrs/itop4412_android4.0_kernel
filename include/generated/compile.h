e3863c63883d2e301554cfdef99e451103437026
r <r@163.com>
2017-02-07 11:41:07 +0800

    init itop4412 android4.0 kernel code

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 7 11:26:24 HKT 2017"
+#define LINUX_COMPILE_BY "r"
+#define LINUX_COMPILE_HOST "r"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
